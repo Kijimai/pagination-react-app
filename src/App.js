228b16d5cdace8656f8857a09cc3b3162c33447c
Jibbyworks <JDBucog@yahoo.com>
2021-09-30 15:32:28 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react"
+import { useFetch } from "./useFetch"
+import Follower from "./Follower"
+
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page, data])
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "...Loading content" : "pagination"}</h1>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower) => (
+            <Follower key={follower.id} {...follower} />
+          ))}
+        </div>
+        {loading ? null : (
+          <div className="btn-container">
+            <button onClick={prevPage} className="prev-btn">
+              Prev
+            </button>
+            {data.map((_, index) => {
+              return (
+                <button
+                  onClick={() => handlePage(index)}
+                  className={`page-btn ${index === page && "active-btn"}`}
+                >
+                  {index + 1}
+                </button>
+              )
+            })}
+            <button onClick={nextPage} className="next-btn">
+              Next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 15
+  const pages = Math.ceil(followers.length / itemsPerPage)
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    //starting value of first item per page
+    const start = index * itemsPerPage
+    return followers.slice(start, start + itemsPerPage)
+  })
+  return newFollowers
+}
 
 export default paginate
